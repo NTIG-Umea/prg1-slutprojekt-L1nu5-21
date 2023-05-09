1599b5dda64465a7f1fea4882a664aca1d064fe4
L1nu5-21 <94972443+L1nu5-21@users.noreply.github.com>
2023-05-09 17:42:47 +0200

    #### 2023-05-09 Added a charachter swap function for swaping two charachters within a string.

--- src/Notebook.md
+++ src/Notebook.md
@@ -1,12 +1,20 @@
 # Project
-
-## Planning
 #### TL;DR version - 2D top-down walking sim in terminal
 
-Simpelt top-down spel där man kan gå runt.
+Simple top-down game where you walk around.
 
-## $$$ Profit $$$
-MoneyGain cheat!
+## Planning
+Fix a simple 20x5-character map for testing in "map.txt". | fixed
+
+Add currentRoomReader and drawRoom methods. | 
+
+Add grid-based movement along the x- and y-axis. |
+
+## Notes
+
+#### 2023-05-09
+Added a charachter swap function for swaping two charachters within a string.
+
 
 ## Post Mortem
 
--- src/main.java
+++ src/main.java
@@ -1,13 +1,17 @@
-import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
-    static boolean running;
+    static int oldX;
+    static int x;
+    static int oldY;
+    static int y;
+    static int currentRoomNumber = 1;
+    Scanner kb = new Scanner(System.in);
     static ArrayList<String> mapList = new ArrayList<>();
+    static ArrayList<String> currentRoom = new ArrayList<>();
 
     public static Scanner createFileScanner() {
         Scanner scan;
@@ -15,12 +19,11 @@ public class main {
             scan = new Scanner(new File("./src/map.txt"));
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-
         }
         return scan;
     }
 
-    public static ArrayList readMap() {
+    public static ArrayList readFile() {
         Scanner skan = createFileScanner();
         while (skan.hasNext()) {
             mapList.add(skan.nextLine());
@@ -28,18 +31,42 @@ public class main {
         return mapList;
     }
 
-    public static void mapDraw() {
+    public static ArrayList readRoom() {
+        for (int i = 0; i < mapList.size(); i++) {
+            if (mapList.get(i).contains("room "+currentRoomNumber)){
+                i++;
+                while (mapList.get(i).contains("x")) {
+                    currentRoom.add(mapList.get(i));
+                    i++;
+                }
+            }
+        }
+        return currentRoom;
+    }
 
+    public static void drawRoom() {
+        for (int i = 0; i < currentRoom.size(); i++) {
+            System.out.println(currentRoom.get(i));
+        }
+
+    }
+
+    public static String swapChar(String in, int p1, int p2) {
+        String ut = "";
+        char ch1C = in.charAt(p1);
+        char ch2C = in.charAt(p2);
+        ut = in.substring(0, p1) + ch2C + in.substring(p1+1, p2) + ch1C;
+        if (in.length() != p2+1) ut += in.substring(p1 + 1);
+        return ut;
     }
 
     public static void main(String[] args) {
-        while (running) {
-            mapDraw();
-            // ask for input
-        }
-    }
-
-    public void keyPressed(KeyEvent e) {
-
+        readFile();
+        readRoom();
+        drawRoom();
+        /*String test = "asliufhj";
+        System.out.println(test);
+        test = swapChar(test,2,4);
+        System.out.println(test);*/
     }
 }
